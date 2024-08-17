9bf4e020665797acf57024d3dfc717db44d11f85
Enes <terdodios@gmail.com>
2024-08-17 14:10:13 +0300

    Initial Commit

---
+++ WatchIt/WatchItApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchItApp.swift
+//  WatchIt
+//
+//  Created by Enes Sarı on 8/17/24.
+//
+
+import SwiftUI
+
+@main
+struct WatchItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
